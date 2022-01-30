b772aa82fef0f18848ede3f976a50994c436d748
Charles <chall70101@gmail.com>
2022-01-30 16:39:27 -0700

    added chef component

--- src/components/Chef.js
+++ src/components/Chef.js
@@ -1,22 +1,18 @@
-import React from "react";
-import {Link} from "react-router-dom"
-import {Card} from "react-bootstrap"
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {Card} from 'react-bootstrap'
+
 
 function Chef({chef}){
+console.log(chef)
     return(
-        <Card key={chef.id}> 
-            <Link 
-            to={`/chefs/${chef.id}`}
-            state={chef}
-            >
-                <div  className="box" >
-                    <h3>{chef.first_name} {chef.last_name}</h3>
-                    <p>insert image tag</p>
-                </div> 
+        <Card>
+            <Link to = {`/chefs/${chef.id}`}>
+                <h2>{chef.first_name + " " +chef.last_name}</h2>
             </Link>
+            
         </Card>
-        
-    );
+    )
 }
 
-export default Chef;
+export default Chef
