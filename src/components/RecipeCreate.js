46b657a914a06bc46ae8fd6ea057ca3ce6e013c8
Charles <chall70101@gmail.com>
2022-01-30 16:42:20 -0700

    added RecipeCreate component

---
+++ src/components/RecipeCreate.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+
+
+function RecipeCreate(){
+
+
+    return (
+        <Container>
+            <h1>Create a Recipe</h1>
+
+        </Container>
+    )
+}
+
+export default RecipeCreate
