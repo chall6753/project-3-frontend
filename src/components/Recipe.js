ab9e27075e80a6f40ed60b6c90e9d40a2d061fa6
Charles <chall70101@gmail.com>
2022-01-30 16:42:01 -0700

    added recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,20 +2,16 @@ import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom"
 import {Card} from "react-bootstrap"
 
-function Recipe({recipe, chefs}){
-    let chef = chefs.filter(chef=> chef.id === recipe.chef_id)[0]
+function Recipe({recipe, chef}){
+    
     
     return(
         <Card>
-            <Link 
-            to={`/recipes/${recipe.id}`}
-            state={{recipe,chefs}}
-            >
-                <div key={recipe.id}>
-                    <h3 >{recipe.recipe_name}</h3>
-                </div>
+            <Link to= {`/recipes/${recipe.id}`}>
+                <h2>{recipe.name}</h2>
             </Link>
-                <h3>Chef: {chef.first_name}</h3>
+            
+            <h3>{chef.first_name}</h3>
         </Card>
     );
 }
