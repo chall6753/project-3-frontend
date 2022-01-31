722c4df6f6e7381a085a54f086d0ee52cb9506bd
Charles <chall70101@gmail.com>
2022-01-30 21:20:17 -0700

    added a deleteRecipe function to update state

--- src/components/App.js
+++ src/components/App.js
@@ -10,18 +10,22 @@ import ChefDetail from './ChefDetail.js'
 import RecipeCreate from './RecipeCreate.js'
 
 function App() {
-const [chefs, setChefs]=useState([])
-const [recipes, setRecipes]=useState([])
+  const [chefs, setChefs]=useState([])
+  const [recipes, setRecipes]=useState([])
 
-useEffect(() =>{
-  fetch("http://localhost:9292/chefs")
-  .then(res => res.json())
-  .then(data => setChefs(data))
-  .then(()=>fetch("http://localhost:9292/recipes"))
-  .then(res => res.json())
-  .then(data=>setRecipes(data))
-},[])
-console.log(recipes)
+  useEffect(() =>{
+    fetch("http://localhost:9292/chefs")
+    .then(res => res.json())
+    .then(data => setChefs(data))
+    .then(()=>fetch("http://localhost:9292/recipes"))
+    .then(res => res.json())
+    .then(data=>setRecipes(data))
+  },[])
+  console.log(recipes)
+  function deleteRecipe(recipe){
+    console.log('recipe from recipeDetail to be deleted', recipe)
+    setRecipes(recipes.filter(r => r.id !== recipe.id))
+  }
   return (
     <div className="main">
       <div className="sidenav">
@@ -31,8 +35,8 @@ console.log(recipes)
       <div className="body"> 
         <Routes>
            <Route exact path='/' element={<Home recipes={recipes} chefs={chefs}/>}/>
-           <Route exact path='/recipes' element={<Recipes recipes={recipes} chefs={chefs}/>}/>
-           <Route exact path='/recipes/:id' element={<RecipeDetail/>}/>
+           <Route exact path='/recipes' element={<Recipes recipes={recipes}/>}/>
+           <Route exact path='/recipes/:id' element={<RecipeDetail deleteRecipe={deleteRecipe}/>}/>
            <Route exact path= '/chefs' element={<Chefs chefs={chefs}/>}/>
            <Route exact path='/chefs/:id' element={<ChefDetail/>}/>
            <Route exact path='/create_recipe' element={<RecipeCreate/>}/>
